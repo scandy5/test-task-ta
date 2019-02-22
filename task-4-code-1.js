e69cbf6a5110a985d86cc222c85020881509da3c
Sergey <scandy_boss@ukr.net>
2019-02-22 15:05:21 +0200

    Create task-4-code-1.js

---
+++ task-4-code-1.js
@@ -0,0 +1,52 @@
+var num10 = 1234; //Используйте 'use scrict', для чего описано в этой статье https://learn.javascript.ru/strict-mode. Для обьявления переменных используйте let или const.
+function toHex(num10) {
+    let num16 = '';
+    for (let i = num10; i > 0; ) { //Выносите объявление переменной за цикл for
+        switch (i % 16) {
+            case 10:
+                {
+                    num16 += 'a';
+                }
+                break;
+            case 11:
+                {
+                    num16 += 'b';
+                }
+                break;
+            case 12:
+                {
+                    num16 += 'c';
+                }
+                break;
+            case 13:
+                {
+                    num16 += 'd';
+                }
+                break;
+            case 14:
+                {
+                    num16 += 'e';
+                }
+                break;
+            case 15: {
+                num16 += 'f';
+            }
+            default: {
+                num16 += i % 16;
+            }
+        }
+        i = i / 16;
+        i = Math.trunc(i); //Строку выше можно обьеденить с этой.
+    }
+    var revurse = '';
+    if (num16 === '') return '';  //Пишите фигурные скобки в if для улучшения читаемости кода.
+    else { 
+        for (let j = num16.length; j >= 0; j--) {
+            revurse += num16.charAt(j);
+        }
+        return revurse;
+    }
+}
+
+var z = toHex(num10); 
+console.log(z);
